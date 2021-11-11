0c72529701d0507573de84a088cf626be393f98e
Jarcob <54203966+X351@users.noreply.github.com>
2021-11-11 14:56:57 -0800

    Update DoE_Login.py

--- DoE_Login.py
+++ DoE_Login.py
@@ -1,10 +1,22 @@
 import requests
+from plyer import notification
+
  
 url = "http://webauth.education.tas.gov.au"
  
-headers = {"Content-Type": "application/json; charset=utf-8", "password": "Password", "username": "UserName"}
+headers = {"Content-Type": "application/json; charset=utf-8", "password": "REPLACE_WITH_PASSWORD", "username": "REPLACE_WITH_USERNAME"}
 
 response = requests.post(url, headers=headers)
- 
-print("Status Code", response.status_code)
 
+if response.status_code == 403:
+    notification.notify(
+    title = 'DoE AutoLogin',
+    message = 'Logged In!',
+    timeout = 2,
+)
+else:
+    notification.notify(
+    title = 'DoE AutoLogin',
+    message = 'Not Logged In :/',
+    timeout = 2,
+)
