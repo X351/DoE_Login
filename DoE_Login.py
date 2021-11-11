1f0b30f7cc450546eeb316a5c024801a11ee8e4f
Jarcob <54203966+X351@users.noreply.github.com>
2021-11-11 15:58:36 -0800

    Update DoE_Login.py
    added auto encoder!

--- DoE_Login.py
+++ DoE_Login.py
@@ -1,10 +1,23 @@
 import requests
+import base64
 from plyer import notification
 
- 
+#put your username and password
+username ="PUT USERNAME"
+password = "PUT PASSWORD"
+
+#NERD SHIT
+
+#Base64 encoder (username)
+username_Byte = base64.b64encode(username.encode("utf-8"))
+username64 = str(username_Byte, "utf-8")
+#(password)
+password_Byte = base64.b64encode(password.encode("utf-8"))
+password64 = str(password_Byte, "utf-8")
+
 url = "http://webauth.education.tas.gov.au"
  
-headers = {"Content-Type": "application/json; charset=utf-8", "password": "REPLACE_WITH_PASSWORD", "username": "REPLACE_WITH_USERNAME"}
+headers = {"Content-Type": "application/json; charset=utf-8", "password": password64, "username": username64}
 
 response = requests.post(url, headers=headers)
 
